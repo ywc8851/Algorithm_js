c0c530fd5fdeb009e6d839fc4d3cf08760d01e16
ywc8851 <ywc8851@naver.com>
2021-10-17 15:57:53 +0900

    docs: 9012 solved
    알고리즘 : 스택
    난이도 : 실버4

--- 백준/9012.js
+++ 백준/9012.js
@@ -1,3 +1,5 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+let n = Number(input[0]);
 function isVPS(str) {
   let stack = [];
   for (let i = 0; i < str.length; i++) {
@@ -13,8 +15,7 @@ function isVPS(str) {
   if (stack.length !== 0) return "NO";
   else return "YES";
 }
-let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-let n = Number(input[0]);
+
 for (let i = 0; i < n; i++) {
   console.log(isVPS(input[i + 1]));
 }
