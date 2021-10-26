3b105de6b754230b76e80b8ef89650937c6dc49f
ywc8851 <ywc8851@naver.com>
2021-10-26 11:24:02 +0900

    docs: 1920 solved
    알고리즘 : 이분탐색
    난이도 : 실버4

---
+++ 백준/1920.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let n = Number(input[0]);
+let nums = input[1].split(" ").map(Number);
+nums.sort((a, b) => a - b);
+let m = Number(input[2]);
+let ans = new Array(m).fill(0);
+
+let find = input[3].split(" ").map(Number);
+for (let i = 0; i < m; i++) {
+  let left = 0;
+  let right = n - 1;
+  let find_num = find[i];
+  while (left <= right) {
+    let mid = parseInt((left + right) / 2);
+    if (nums[mid] === find_num) {
+      ans[i] = 1;
+      break;
+    }
+    if (nums[mid] > find_num) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  console.log(ans[i]);
+}
