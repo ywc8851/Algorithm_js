cf3d45948d9b0b96f66e8dff04ffcff34c9e37a5
ywc8851 <ywc8851@naver.com>
2021-11-13 23:49:34 +0900

    docs: 394 solved
    알고리즘 : 자료구조(stack)
    난이도 : medium

---
+++ 리트코드/394.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+     let stack = [];
+
+    for (const x of s) {
+        let string = "";
+        if (x !== "]") stack.push(x);
+        else {
+            while (stack[stack.length - 1] !== "[") {
+                let tmp = stack.pop();
+                string = tmp + string;
+            }
+            stack.pop();
+            let num = "";
+            while (!isNaN(stack[stack.length - 1])) {
+                const numTmp = stack.pop();
+                num = numTmp + num;
+            }
+            stack.push(string.repeat(Number(num)));
+        }
+    }
+    return stack.join("");
+};
