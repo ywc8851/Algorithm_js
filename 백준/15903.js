d7b92238f148360b8ff2b0f809d42fcada2130b4
ywc8851 <ywc8851@naver.com>
2022-06-09 22:25:59 +0900

    docs: 15903 solved
    알고리즘 : 그리디
    난이도 : 실버 1

---
+++ 백준/15903.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+class minHeap {
+  constructor() {
+    this.heap = [];
+    this.heap.push(-Infinity);
+  }
+  insert(val) {
+    this.heap.push(val);
+    this.upheap(this.heap.length - 1);
+  }
+  upheap(pos) {
+    let tmp = this.heap[pos];
+    while (tmp < this.heap[parseInt(pos / 2)]) {
+      this.heap[pos] = this.heap[parseInt(pos / 2)];
+      pos = parseInt(pos / 2);
+    }
+    this.heap[pos] = tmp;
+  }
+  get() {
+    if (this.heap.length === 2) return this.heap.pop();
+    let res = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    this.downheap(1, this.heap.length - 1);
+    return res;
+  }
+  downheap(pos, len) {
+    let tmp = this.heap[pos],
+      child;
+    while (pos <= parseInt(len / 2)) {
+      child = pos * 2;
+      if (child < len && this.heap[child] > this.heap[child + 1]) child++;
+      if (tmp <= this.heap[child]) break;
+      this.heap[pos] = this.heap[child];
+      pos = child;
+    }
+    this.heap[pos] = tmp;
+  }
+  size() {
+    return this.heap.length - 1;
+  }
+  front() {
+    return this.heap[1];
+  }
+}
+let [, m] = input[0].split(" ").map(Number);
+const pq = new minHeap();
+input[1]
+  .split(" ")
+  .map(BigInt)
+  .map((num) => pq.insert(num));
+
+while (m--) {
+  const a = pq.get();
+  const b = pq.get();
+
+  const newNum = a + b;
+  pq.insert(newNum);
+  pq.insert(newNum);
+}
+
+let sum = BigInt(0);
+while (pq.size()) {
+  sum += pq.get();
+}
+
+console.log(sum.toString());
