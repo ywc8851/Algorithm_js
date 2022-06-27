cea3c4f6d01355f3c234d43e00837f167425db7c
ywc8851 <ywc8851@naver.com>
2022-06-27 12:46:30 +0900

    docs: 2193 solved
    알고리즘 : DP
    난이도 : 실버 3

--- 백준/2193.js
+++ 백준/2193.js
@@ -1,10 +1,10 @@
-const fs = require('fs')
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './test.txt'
-let input = fs.readFileSync(filePath).toString().trim().split("\n")[0]
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const findPinaryNumber = len => {
-  const pinaryArr = new Array(len).fill(1n)
-  for (let i = 2; i < len; i++) pinaryArr[i] = pinaryArr[i - 1] + pinaryArr[i - 2]
-  return (pinaryArr[len - 1] + '')
+const n = +input[0];
+const arr = [0, 1, 1];
+for (let i = 3; i <= n; i++) {
+  arr[i] = BigInt(arr[i - 1]) + BigInt(arr[i - 2]);
 }
-console.log(findPinaryNumber(+input))
+console.log(BigInt(arr[n]).toString());
