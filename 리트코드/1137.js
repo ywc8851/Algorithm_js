f5f303bd78c2f7e30d0d8fc6b7c5f52884ccabc4
ywc8851 <ywc8851@naver.com>
2023-01-30 12:36:11 +0900

    docs: 리트코드 1137 solved
    난이도 : Easy

---
+++ 리트코드/1137.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function (n) {
+  const arr = Array.from({ length: n + 1 }, () => 0);
+  arr[0] = 0;
+  arr[1] = 1;
+  arr[2] = 1;
+
+  for (let i = 3; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+  }
+
+  return arr[n];
+};
